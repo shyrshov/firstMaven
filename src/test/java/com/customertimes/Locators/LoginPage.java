8cfc836bb3f10e0c0c61e8bae3a1d786ebb9705f
Andrii <a.shyrshov@gmail.com>
2021-05-22 21:19:54 +0300

    Third part1

---
+++ src/test/java/com/customertimes/Locators/LoginPage.java
@@ -0,0 +1,24 @@
+package com.customertimes.Locators;
+
+public class LoginPage {
+    private String LoginCss = "mat-card h1";
+    private String LoginXpath = "//h1[text()='Login']";
+    private String EmailCss = "[name=email]";
+    private String EmailXpath = "//*[@name='email']";
+    private String PasswordCss = "[name=password]";
+    private String PasswordXpath = "//*[@name='password']";
+    private String DisplayPasswordCss = "[aria-label='Button to display the password']";
+    private String DisplayPasswordXpath = "//*[@aria-label='Button to display the password']";
+    private String LogInButtonCss = "button[type=submit]";
+    private String LogInButtonXpath = "//button[@type='submit']";
+    private String LogInIconCss = "#loginButton .mat-icon.notranslate.material-icons.mat-icon-no-color";
+    private String LogInIconXpath = "//span/mat-icon[text()=' exit_to_app ']";
+    private String RememberMeCheckboxCss = "span.mat-checkbox-inner-container";
+    private String RememberMeCheckboxXpath = "//span[@class='mat-checkbox-inner-container']";
+    private String RememberLabelCss = "span.mat-checkbox-label";
+    private String RememberMeLabelXpath = "//span[@class='mat-checkbox-label']";
+    private String RegisterCss = "[href='#/register']";
+    private String RegisterXpath = "//*[@href='#/register']";
+
+
+}
---
+++ src/test/java/com/customertimes/Locators/Registration.java
@@ -0,0 +1,31 @@
+package com.customertimes.Locators;
+
+public class Registration {
+    private String UserRegistrationCss = "div h1";
+    private String UserRegistrationXpath = "//mat-card/h1";
+    private String EmailCss = "input#emailControl";
+    private String EmailXpath = "//input[@id='emailControl']";
+    private String PasswordCss =  "input#passwordControl";
+    private String PasswordXpath = "//input[@id='passwordControl']";
+    private String PasswordPromptIconCss =  "#mat-hint-0 svg";
+    private String PasswordPromptIconXpath = "//*[@id='mat-hint-0']/*[@data-icon='exclamation-circle']";
+    private String PasswordPromptCss =  "#mat-hint-0 em";
+    private String PasswordPromptXpath = "//*[text()='Password must be 5-20 characters long.']";
+    private String PasswordCounterCss =  "#mat-hint-1";
+    private String PasswordCounterXpath = "//*[@id='mat-hint-1']";
+    private String RepeatPasswordCss =  "input#repeatPasswordControl";
+    private String RepeatPasswordXpath = "//input[@id='repeatPasswordControl']";
+    private String RepeatPasswordCounterCss =  "#mat-hint-2";
+    private String RepeatPasswordCounterXpath = "//*[@id='mat-hint-2']";
+    private String SlideBarCss =  "label .mat-slide-toggle-bar";
+    private String SlideBarXpath = "//label/div[@class='mat-slide-toggle-bar']";
+    private String SlideBarTextCss =  "span.mat-slide-toggle-content";
+    private String SlideBarTextXpath = "//span[@class='mat-slide-toggle-content']";
+    private String ProgressBarTextCss =  "mat-progress-bar div div:first-of-type";
+    private String ProgressBarTextXpath = "//mat-progress-bar/div/div[1]";
+
+
+
+
+
+}
